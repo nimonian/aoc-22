658b4e39ba5f78d090aaea7ed0f25c446a1cd182
si-mv <simon.haines@multiverse.io>
2022-12-03 08:07:28 +0000

    formatting

--- 01-solution.py
+++ 01-solution.py
@@ -1,12 +1,14 @@
+solution = {'a': 0, 'b': 0}
+
 with open('01-input.txt', 'r') as f:
-  elves = f.read().strip().split('\n\n')
+    elves = f.read().strip().split('\n\n')
 
 elves = [elf.strip().split('\n') for elf in elves]
 elves = [sum([int(x) for x in elf]) for elf in elves]
 
-# part a
-print(max(elves))
+solution['a'] = max(elves)
 
-# part b
 elves.sort()
-print(sum(elves[-3:]))
+solution['b'] = sum(elves[-3:])
+
+print(solution)
