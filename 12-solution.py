fc40856726166c4378f7be764fb472b8232dcdec
si-mv <simon.haines@multiverse.io>
2022-12-14 09:22:42 +0000

    solves 12b very slowly

--- 12-solution.py
+++ 12-solution.py
@@ -2,27 +2,33 @@ import math
 
 solution = {'a': 0, 'b': 0}
 
+
 with open('./12-input.txt', 'r') as f:
-    M = []
-    for (r, row) in enumerate([l.strip() for l in f.readlines()]):
-        M.append([])
-        for (c, char) in enumerate(row):
-            node = {
-                'char': char,
-                'height': ord(char) - 96,
-                'distance': math.inf,
-                'visited': False,
-                'position': (r, c)
-            }
-            if char == 'S':
-                node['char'] = 'a'
-                node['height'] = ord('a') - 96
-                node['distance'] = 0
-            if char == 'E':
-                node['char'] = 'z'
-                node['height'] = ord('z') - 96
-                E = node
-            M[-1].append(node)
+    lines = [l.strip() for l in f.readlines()]
+
+M = []
+for (r, row) in enumerate(lines):
+    M.append([])
+    for (c, char) in enumerate(row):
+        node = {
+            'char': char,
+            'height': ord(char) - 96,
+            'distance': math.inf,
+            'visited': False,
+            'position': (r, c)
+        }
+        if char == 'S':
+            node['height'] = ord('a') - 96
+        if char == 'E':
+            node['height'] = ord('z') - 96
+            node['stop'] = True
+            E = node
+        M[-1].append(node)
+
+def reset_map():
+    for node in get_nodes(M):
+        node['distance'] = math.inf
+        node['visited'] = False
 
 # add two tuples vector style, e.g. (1, 2) + (3, 4) = (4, 6)
 def v_add(u, v):
@@ -31,6 +37,10 @@ def v_add(u, v):
 # get the node dict at a given position in the map
 def get_node(p):
     return M[p[0]][p[1]]
+
+# flat map the map :/
+def get_nodes(M):
+    return [n for r in M for n in r]
 
 # get a list of all unvisited neighbours represented as nodes
 def get_neighbours(n):
@@ -43,17 +53,31 @@ def get_neighbours(n):
     return N
 
 # do the hustle!
-def dijkstra_a():
-    nodes = [P for r in M for P in r]
-    while E['visited'] == False:
+def dijkstra(S = None):
+    nodes = get_nodes(M)
+    if not S:
+        S = next(filter(lambda n: n['char'] == 'S', nodes))
+    S['distance'] = 0
+    while not E['visited']:
         nodes.sort(key=lambda n: n['distance'])
         n = nodes.pop(0)
         n['visited'] = True
         for m in get_neighbours(n):
             m['distance'] = min(m['distance'], n['distance'] + 1)
-    return n['distance']
+    d = n['distance']
+    reset_map()
+    return d
 
+def a():
+    return dijkstra()
 
-solution['a'] = dijkstra_a()
+def b():
+    res = math.inf
+    for a in filter(lambda n: n['char'] in ['a', 'S'], get_nodes(M)):
+        res = min(dijkstra(a), res)
+    return res
+
+solution['a'] = a()
+solution['b'] = b()
 
 print(solution)
